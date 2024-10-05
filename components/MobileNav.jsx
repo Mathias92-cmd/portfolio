b9aaa71cf16091ef57cc68fbdeb80ccc8ae007ac
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-10-05 10:29:11 +0200

    Mise en place navbar responsive

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -3,7 +3,8 @@
 import { Sheet , SheetContent , SheetTrigger } from '@/components/ui/sheet';
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {} from 'react-icons/ci';
+import {CiMenuFries} from 'react-icons/ci';
+
 
 const Links = [
     {
@@ -16,16 +17,44 @@ const Links = [
         name: 'resume',
         path: '/resume',
     },{
-        name: 'work',
-        path: '/work',
+        name: 'projet',
+        path: '/projet',
     },{
         name: 'contact',
         path: '/contact',
-    },
+    }
 ];
 
 const MobileNav = () => {
-    return <nav>Mobilenav</nav>
+    const pathname = usePathname();
+    return <Sheet>
+        <SheetTrigger className="flex justify-center items-center">
+            <CiMenuFries className="text-[32px] text-accent" />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col">
+            <div className="mt-32 mb-40 text-center text-2xl">
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Mathias <span className="text-accent">.</span>
+                    </h1>
+                </Link>
+            </div>
+            <nav className="flex flex-col justify-center items-center gap-8">
+                {Links.map((link, index)  => {
+                    return (
+                    <Link
+                        href={link.path}
+                        key={index}
+                        className={`${link.path === pathname && "text-accent border-b-2 border-accent"}
+                        text-xl capitalize hover:text-accent transition-all`}
+                    >
+                        {link.name}
+                    </Link>
+                    );
+                })};
+            </nav>
+        </SheetContent>
+    </Sheet>
 }
 
 export default MobileNav;
