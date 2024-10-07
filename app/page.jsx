7b88df2cd891f35215474a039e9140e8115927c5
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-10-07 18:41:18 +0200

    Ajout skills

--- app/page.jsx
+++ app/page.jsx
@@ -7,12 +7,12 @@ import Stats from "@/components/Stats";
 const Home = () => {
     return (
         <section className="h-full flex flex-col justify-center items-center">
-            <div className="container mx-auto h-full">
-                <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-                    <div className="text-center xl:text-left order-2 xl:order-none">
+            <div className="container mx-auto h-full px-4 xl:px-0">
+                <div className="flex flex-col items-center xl:flex-row xl:items-start xl:justify-between xl:pt-8 xl:pb-24">
+                    <div className="order-2 xl:order-none text-center xl:text-left">
                         <span>Developpeur Junior</span>
                         <h1 className="h1 mb-6">
-                            Bonjour je suis <br/> <span className="text-accent">Morel Mathias</span>
+                            Bonjour, je suis <br/> <span className="text-accent">Morel Mathias</span>
                         </h1>
                         <p className="max-w-[500px] mb-9 text-white/80">
                             Je suis un étudiant en BUT informatique à l&#39;Université de Lens. Je suis passionné par le
@@ -21,7 +21,7 @@ const Home = () => {
                         <div className="flex flex-col xl:flex-row items-center gap-8 xl:gap-4">
                             <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                                 <span>Téléchargez mon CV</span>
-                                <FiDownload className="text-xl" />
+                                <FiDownload className="text-4xl" />
                             </Button>
                             <div className="mb-8 xl:mb-0">
                                 <Social
@@ -31,7 +31,7 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="order-1 xl:order-none mb-8 xl:mb-8">
+                    <div className="order-1 xl:order-none mb-8 xl:mb-0">
                         <Photo />
                     </div>
                 </div>
--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,90 @@
+"use client";
+
+import {faHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs, FaHtml5, FaPython, FaPhp} from 'react-icons/fa';
+
+import { SiTailwindcss , SiNextdotjs } from "react-icons/si";
+
+const about = {
+    title: "About Me",
+    description: "Lorem",
+    info: [
+        {
+            fieldName: "Name",
+            fielValue: "Morel Mathias"
+        },
+        {
+            fieldName: "Phone",
+            fielValue: "(+33) 6 35 29 22 18"
+        },
+        {
+            fieldName: "GitHub",
+            fielValue: "Mathias92-cmd"
+        },
+        {
+            fieldName: "Nationalité",
+            fielValue: "Française"
+        },
+        {
+            fieldName: "Email",
+            fielValue: "mathias_morel@ens.univ-artois.fr"
+        },
+        {
+            fieldName: "Langages",
+            fielValue: "Anglais / Espagnol",
+        },
+    ]
+};
+
+const experience = {
+    icon: "👨‍💻",
+    title: "Experience",
+    description: "Lorem",
+    items: [
+        {
+            etablissement: "Université de Lens",
+            poste: "Etudiant en BUT informatique",
+            date: "2023 - Present",
+        },
+        {
+            etablissement: "Lycee Henri Darras",
+            poste: "Baccalauréat technologique sciences et technologies de l'industrie et du développement durable ( option SIN )",
+            date: "2021 - 2023",
+        },
+        {
+            etablissement: "College Anita Conti",
+            poste: "Brevet des collèges",
+            date: "2017 - 2021",
+        }
+    ]
+};
+
+const skills = {
+    icon: "🛠",
+    title: "Skills",
+    description: "Lorem",
+    skillList: [
+        {
+            icon: <FaHtml5 />,
+            title: "HTML"
+        },
+        {
+            icon: <FaCss3 />,
+            title: "CSS"
+        },
+        {
+            icon: <FaPython />,
+            title: "Python"
+        },
+        {
+            icon: <FaPhp />,
+            title: "PHP"
+        },
+        {
+            icon: <FaReact />,
+            title: "React"
+        }
+    ]
+};
 
 
 const Resume = () => {
