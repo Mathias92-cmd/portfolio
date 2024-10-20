d78427b4982f6dbc52538685783618ab2a49a785
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-10-20 09:44:12 +0200

    Mise en place du swiper

--- app/projet/page.jsx
+++ app/projet/page.jsx
@@ -3,19 +3,24 @@
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import "swiper/css";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import Link from 'next/link';
+import Image from 'next/image';
+
 const projects = [
     {
         num: '1',
         category: 'backend',
         title: 'Projet 1',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet purus in urna tincidunt luctus. Sed sit amet purus in urna tincidunt luctus. Sed sit amet purus in urn',
-        techs: ['Node.js', 'Express', 'MongoDB'],
-        image: '/',
-        github: "",
+        techs: ['Python', 'Django'],
+        image: '/tabBord.PNG',
+        github: "https://github.com/Mathias92-cmd/TableauDeBord",
     },
     {
         num: '2',
@@ -46,13 +51,14 @@ const projects = [
     }
 ]
 
-/**
- * Projet component
- * @returns {JSX.Element}
- * @constructor
- */
 const Projet = () => {
     const [project, setProject] = useState(projects[0]);
+
+    const handleSlideChange = (swiper) => {
+        const currentSlide = swiper.activeIndex;
+        setProject(projects[currentSlide]);
+    }
+
     return (
         <motion.section initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
@@ -76,15 +82,14 @@ const Projet = () => {
                                 </li>
                             ))}
                         </ul>
-                        <div className="border border-white/20">
+                        <div className="border border-white/20 mt-4 p-2 flex gap-4">
                         </div>
-                        <div className="flex items-center gap-4 mt-2">
+                        <div className="flex items-center gap-4 mt-4">
                             <Link href={project.title}>
                                 <TooltipProvider delayDuration={100}>
                                     <Tooltip>
-                                        <TooltipTrigger
-                                            className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                            <BsArrowUpRight className="text-3xl group-hover:text-accent"/>
+                                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                            <BsArrowUpRight className="text-3xl group-hover:text-accent" />
                                         </TooltipTrigger>
                                         <TooltipContent>
                                             Voir le projet
@@ -95,9 +100,8 @@ const Projet = () => {
                             <Link href={project.github}>
                                 <TooltipProvider delayDuration={100}>
                                     <Tooltip>
-                                        <TooltipTrigger
-                                            className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                            <BsGithub className="text-3xl group-hover:text-accent"/>
+                                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                            <BsGithub className="text-3xl group-hover:text-accent" />
                                         </TooltipTrigger>
                                         <TooltipContent>
                                             Voir le code
@@ -107,6 +111,27 @@ const Projet = () => {
                             </Link>
                         </div>
                     </div>
+                    <Swiper
+                        spaceBetween={30}
+                        slidesPerView={1}
+                        className="w-full h-[500px] mb-12"
+                        onSlideChange={handleSlideChange}
+                        modules={[Navigation, Pagination]}
+                        navigation
+                        pagination={{ clickable: true }}
+                    >
+                        {projects.map((project, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="w-full h-[500px] relative group flex justify-center items-center bg-blue-500">
+                                    <Image src={project.image}
+                                           fill
+                                           className="object-cover"
+                                           alt=""
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </motion.section>
